819ebba0f6201ff038582e7c1c0290fc00d9ab84
Locais <flowuniao@gmail.com>
2026-08-04 23:01:48 -0400

    fix: resolve politica de segurança do VPS

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://pagead2.googlesyndication.com; connect-src 'self' https://api.centraleti.com.br https://*.supabase.co; img-src 'self' data: https://fotos.centraleti.com.br https://f.centraleti.com.br;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;

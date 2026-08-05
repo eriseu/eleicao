7ca15b8ecb74bd8b69e8cb17fdaa6cba5b853974
Locais <flowuniao@gmail.com>
2026-08-04 23:05:15 -0400

    fix: resolve politica de segurança do supabase e outros

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,14 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://pagead2.googlesyndication.com; connect-src 'self' https://api.centraleti.com.br https://*.supabase.co; img-src 'self' data: https://fotos.centraleti.com.br https://f.centraleti.com.br;",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://pagead2.googlesyndication.com https://partner.googleadservices.com https://tpc.googlesyndication.com",
+              "style-src 'self' 'unsafe-inline'",
+              "connect-src 'self' https://api.centraleti.com.br https://*.supabase.co https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net https://ep1.adtrafficquality.google",
+              "img-src 'self' data: https://fotos.centraleti.com.br https://f.centraleti.com.br https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net",
+              "frame-src 'self' https://googleads.g.doubleclick.net https://tpc.googlesyndication.com",
+            ].join("; "),
           },
         ],
       },

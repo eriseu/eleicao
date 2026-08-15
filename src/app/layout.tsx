31069e2c3bb75aa4f3d14a3c1e8529b96edf7d4c
Locais <flowuniao@gmail.com>
2026-08-14 23:30:37 -0400

    Atualiza o script adsense, dentro do head do layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,8 +51,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0388943655208566"
-         crossorigin="anonymous"></script>
+        <script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-..."
+          crossOrigin="anonymous">
+        </script>
       <head />
       <body className="antialiased">
         {/* Google Analytics */}

7fc56e636f37a801650ace67461b456c010fd5f2
Locais <flowuniao@gmail.com>
2026-08-19 14:01:08 -0400

    Adiciona o script do adsense corretamente em layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,11 +50,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-        <script 
-          async 
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-..."
-          crossOrigin="anonymous">
-        </script>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0388943655208566"
+     	crossorigin="anonymous"></script>
       <head />
       <body className="antialiased">
         {/* Google Analytics */}
